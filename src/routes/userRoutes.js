36e50e82b531411a339c646609d74250c511d3e3
digitalcreationsteam <dcteam1999@gmail.com>
2025-12-19 15:28:35 +0530

    merge

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -29,7 +29,7 @@ router.get("/profile", protect, (req, res) => {
 // =================================================
 // DASHBOARD
 // =================================================
-router.get("/dashboard/:userId", getDashboardByUserId);
+router.get("/dashboard", getDashboardByUserId);
 router.get("/experience_index", getDashboardByUserId);
 
 // =================================================
