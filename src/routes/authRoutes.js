0e6fd98032ca159f1b6b011d2713d61197189de4
digitalcreationsteam <dcteam1999@gmail.com>
2025-12-29 14:15:27 +0530

    merge

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -21,19 +21,17 @@ router.post("/resend-verification", resendVerificationEmail);
 // =================================================
 // GOOGLE OAUTH (STUDENT ONLY)
 // =================================================
-router.get(
-  "/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
+
+router.get("/google", passport.authenticate("google", {
+  scope: ["profile", "email"]
+}));
 
 router.get(
   "/google/callback",
   passport.authenticate("google", { session: false }),
   (req, res) => {
     res.redirect(
-      `http://localhost:5000/login-success?token=${req.user.token}`
+      `http://localhost:3000/login-success?token=${req.user.token}`
     );
   }
 );
