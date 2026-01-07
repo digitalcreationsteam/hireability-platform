162a76aa853b56394f4ee56184c01eb2d107c2b0
maher-sandip <Rajputsandipm44@gmail.com>
2026-01-07 17:43:47 +0530

    Log In with LinkedIn

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,4 +1,5 @@
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+// const LinkedInStrategy = require("passport-linkedin-oauth2").Strategy; //// Linkedin
 const passport = require("passport");
 const User = require("../models/userModel");
 const generateToken = require("../utils/generateToken");
@@ -38,4 +39,49 @@ passport.use(
     }
   )
 );
+
+///LinkedIN:
+
+// passport.use(
+//   new LinkedInStrategy(
+//     {
+//       clientID: process.env.LINKEDIN_CLIENT_ID,
+//       clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+//       callbackURL: process.env.LINKEDIN_CALLBACK_URL,
+//       scope: ["openid", "profile", "email"],
+//     },
+//     async (accessToken, refreshToken, profile, done) => {
+//       try {
+//         const email =
+//           profile.emails?.[0]?.value || profile._json?.email || null;
+
+//         if (!email) {
+//           return done(new Error("LinkedIn email not available"), null);
+//         }
+
+//         let user = await User.findOne({ email });
+
+//         if (!user) {
+//           user = await User.create({
+//             firstname: profile.name?.givenName || "LinkedIn",
+//             lastname: profile.name?.familyName || "User",
+//             email,
+//             role: "student",
+//             socialLogin: "linkedin",
+//             linkedinId: profile.id,
+//             isVerified: true,
+//             password: "linkedin_oauth",
+//           });
+//         }
+
+//         const token = generateToken(user);
+//         user.token = token;
+
+//         done(null, user);
+//       } catch (err) {
+//         done(err, null);
+//       }
+//     }
+//   )
+// );
 
