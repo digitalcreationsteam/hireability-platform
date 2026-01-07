0ce694640b4fcc5e9a3439664fe7123377a08eef
digitalcreationsteam <dcteam1999@gmail.com>
2026-01-07 17:33:03 +0530

    merge

--- src/models/userScoreModel.js
+++ src/models/userScoreModel.js
@@ -8,6 +8,18 @@ const userScoreSchema = new mongoose.Schema({
     required: true
   },
 
+  city: String,
+  cityRank: Number,
+
+  state: String,
+  stateRank: Number,
+
+  country: String,
+  countryRank: Number,
+
+  global: String,
+  globalRank: Number,
+
   educationScore: { type: Number, default: 0 },
   workScore: { type: Number, default: 0 },
   certificationScore: { type: Number, default: 0 },
@@ -19,5 +31,18 @@ const userScoreSchema = new mongoose.Schema({
   hireabilityIndex: { type: Number, default: 0 }
 
 }, { timestamps: true });
+
+/* 🔥 Trigger rank recalculation AFTER save/update */
+userScoreSchema.post("save", async function () {
+  const { recalculateRanks } = require("../services/rankService");
+  await recalculateRanks(this.userId);
+});
+
+userScoreSchema.post("findOneAndUpdate", async function (doc) {
+  if (doc) {
+    const { recalculateRanks } = require("../services/rankService");
+    await recalculateRanks(doc.userId);
+  }
+});
 
 module.exports = mongoose.model("UserScore", userScoreSchema);
