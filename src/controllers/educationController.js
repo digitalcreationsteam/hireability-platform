1aa2b0bd2c93881484a65ab23f5e868f8b58e199
digitalcreationsteam <dcteam1999@gmail.com>
2026-02-14 16:26:34 +0530

    merge

--- src/controllers/educationController.js
+++ src/controllers/educationController.js
@@ -1,7 +1,7 @@
 const Education = require("../models/educationModel");
 const User = require("../models/userModel");
 const { recalculateUserScore } = require("../services/recalculateUserScore");
-
+const UserScore = require("../models/userScoreModel");
 /* ==================================================
    SINGLE EDUCATION SCORE CALCULATION
 ================================================== */
