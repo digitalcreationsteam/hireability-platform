a615917c20ed15c80be38c66fd9da95e37540119
DC Team <dcteam1999@gmail.com>
2026-02-16 18:28:19 +0530

    case study updated

---
+++ src/models/userCaseAttemptModel copy.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const userCaseAttemptSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  caseId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "CaseStudy",
+    required: true
+  },
+  attemptNumber: {
+    type: Number,
+    required: true
+  },
+  currentQuestion: {
+    type: Number,
+    default: 1
+  },
+  answers: [
+    {
+      questionId: mongoose.Schema.Types.ObjectId,
+      selectedOption: String
+    }
+  ],
+  score: {
+    type: Number,
+    default: 0
+  },
+  isCompleted: {
+    type: Boolean,
+    default: false
+  },
+   isSubmitted: {   // 🔥 ADD THIS
+    type: Boolean,
+    default: false
+  },
+  revealUnlocked: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("UserCaseAttempt", userCaseAttemptSchema);
